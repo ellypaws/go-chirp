9ed6d3ca0e69fac12bb0463d794d423a6384ab84
Elly <145195760+ellypaws@users.noreply.github.com>
2025-01-10 22:10:48 +0800

    refactor: use /api/v1 prefix

--- cmd/main.go
+++ cmd/main.go
@@ -20,12 +20,13 @@ func main() {
 	db.InitDB()
 
 	router := mux.NewRouter()
-	router.HandleFunc("/signup", handlers.SignupHandler).Methods("POST")
-	router.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
-	router.HandleFunc("/tweet", middleware.JWTMiddleware(http.HandlerFunc(handlers.CreateTweetHandler)).ServeHTTP).Methods("POST")
-	router.HandleFunc("/follow", middleware.JWTMiddleware(http.HandlerFunc(handlers.FollowHandler)).ServeHTTP).Methods("POST")
-	router.HandleFunc("/user/{userID}/tweets", middleware.JWTMiddleware(http.HandlerFunc(handlers.FetchUserTweetsHandler)).ServeHTTP).Methods("GET")
-	router.HandleFunc("/username/{username}/tweets", handlers.FetchUserTweetsHandler).Methods("GET")
+	v1 := router.PathPrefix("/api/v1").Subrouter()
+	v1.HandleFunc("/signup", handlers.SignupHandler).Methods("POST")
+	v1.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
+	v1.HandleFunc("/tweet", middleware.JWTMiddleware(http.HandlerFunc(handlers.CreateTweetHandler)).ServeHTTP).Methods("POST")
+	v1.HandleFunc("/follow", middleware.JWTMiddleware(http.HandlerFunc(handlers.FollowHandler)).ServeHTTP).Methods("POST")
+	v1.HandleFunc("/user/{userID}/tweets", middleware.JWTMiddleware(http.HandlerFunc(handlers.FetchUserTweetsHandler)).ServeHTTP).Methods("GET")
+	v1.HandleFunc("/username/{username}/tweets", handlers.FetchUserTweetsHandler).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
